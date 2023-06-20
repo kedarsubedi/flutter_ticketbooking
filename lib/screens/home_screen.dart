26027d495094aa5da7e6604ab971d25197d2fbbe
kedarsubedi <kedar.sed@gmail.com>
2023-06-20 15:52:32 +0545

    layout: home page layout is completed

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:booktickets/screens/hotel_screen.dart';
+import 'package:booktickets/screens/ticket_view.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -80,7 +82,45 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
-
+          const Gap(15),
+          const SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            padding: const EdgeInsets.only(left: 20),
+            child: Row(
+              children: [
+                TicketView(),
+                TicketView(),
+              ],
+            ),
+          ),
+          const Gap(15),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Hotels", style: Styles.headLineStyle2,),
+                InkWell(
+                    onTap: () {
+                      print("You are tapped");
+                    },
+                    child: Text("View all", style: Styles.textStyle.copyWith(color: Styles.primaryColor),)),
+              ],
+            ),
+          ),
+          const Gap(15),
+          SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20),
+              child: Row(
+                children: [
+                  HotelScreen(),
+                  HotelScreen(),
+                  HotelScreen(),
+                  HotelScreen(),
+                ],
+              ),
+          ),
         ],
       ),
     );
