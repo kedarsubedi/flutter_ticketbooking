8512abbce5c79411d96ca8afc61036109f0b437c
kedarsubedi <kedar.sed@gmail.com>
2023-07-07 16:48:50 +0545

    feature: tickets page completed

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,19 @@
+import 'package:booktickets/utils/app_styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Styles.bgColor,
+      body: ListView(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/ticket_view.dart
+++ lib/screens/ticket_view.dart
@@ -1,8 +1,11 @@
 import 'package:booktickets/utils/app_layout.dart';
+import 'package:booktickets/widgets/column_layout.dart';
+import 'package:booktickets/widgets/layout_builder_widget.dart';
 import 'package:booktickets/widgets/thick_container.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
+import 'package:get/get.dart';
 
 import '../utils/app_styles.dart';
 
@@ -16,10 +19,11 @@ class TicketView extends StatelessWidget {
     final size = AppLayout.getSize(context);
     return SizedBox(
       width: size.width*0.85,
-      height: AppLayout.getHeight(200),
+      height: AppLayout.getHeight(GetPlatform.isAndroid==true?167:169),
       child: Container(
         margin: EdgeInsets.only(right: AppLayout.getHeight(16)),
         child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
             /*
             showing the blue part of the ticket
@@ -43,22 +47,7 @@ class TicketView extends StatelessWidget {
                         children: [
                           SizedBox(
                             height: AppLayout.getHeight(24),
-                            child: LayoutBuilder(
-                              builder: (BuildContext context, BoxConstraints constraints) {
-                                print("The widht is ${constraints.constrainWidth()}");
-                                return Flex(
-                                  direction: Axis.horizontal,
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  mainAxisSize: MainAxisSize.max,
-                                  children: List.generate((constraints.constrainWidth()/6).floor(), (index) => SizedBox(
-                                    width: 3, height: 1, child: DecoratedBox(decoration: BoxDecoration(
-                                      color: isColor==null? Colors.white:Colors.grey.shade300
-                                  ),),
-                                  )),
-                                );
-                              },
-
-                            ),
+                            child: const AppLayoutBuilderWidget(sections: 6,),
                           ),
                           Center(child: Transform.rotate(
                             angle: 1.5, child: Icon(Icons.local_airport_rounded, color: isColor==null? Colors.white:Color(0xFF8ACCF7),),
@@ -149,7 +138,7 @@ class TicketView extends StatelessWidget {
             Container(
               decoration: BoxDecoration(
                 color: isColor==null?Styles.orangeColor:Colors.white,
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(AppLayout.getHeight(21)), bottomRight: Radius.circular(AppLayout.getHeight(21)),),
+                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(isColor==null?21:0), bottomRight: Radius.circular(isColor==null?21:0),),
               ),
               padding: const EdgeInsets.only(left: 16, top: 10, right: 16, bottom: 16),
               child: Column(
@@ -157,36 +146,9 @@ class TicketView extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(ticket['date'],
-                          style: isColor==null?Styles.headLineStyle3.copyWith(color: Colors.white):Styles.headLineStyle3,),
-                          const Gap(5),
-                          Text("Date",
-                          style: isColor==null?Styles.headLineStyle4.copyWith(color: Colors.white):Styles.headLineStyle4,)
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(ticket['departure_time'],
-                          style: isColor==null?Styles.headLineStyle3.copyWith(color: Colors.white):Styles.headLineStyle3,),
-                          const Gap(5),
-                          Text("Departure time",
-                          style: isColor==null?Styles.headLineStyle4.copyWith(color: Colors.white):Styles.headLineStyle4,)
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(ticket['number'].toString(),
-                          style: isColor==null?Styles.headLineStyle3.copyWith(color: Colors.white):Styles.headLineStyle3,),
-                          const Gap(5),
-                          Text("Number",
-                          style: isColor==null?Styles.headLineStyle4.copyWith(color: Colors.white):Styles.headLineStyle4,)
-                        ],
-                      ),
+                      AppColumnLayout(firstText: ticket['date'], secondText: "Date", alignment: CrossAxisAlignment.start, isColor: isColor,),
+                      AppColumnLayout(firstText: ticket['departure_time'], secondText: "Departure time", alignment: CrossAxisAlignment.center, isColor: isColor,),
+                      AppColumnLayout(firstText: ticket['number'].toString(), secondText: "Number", alignment: CrossAxisAlignment.end, isColor: isColor,),
                     ],
                   )
                 ],
